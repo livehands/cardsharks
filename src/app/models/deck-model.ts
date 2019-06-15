3c0fed41c197676c36796fba4a7295aa2332ba0c
Karim Kameka <kamekk@hotmail.com>
2019-06-15 11:28:00 -0400

    fixed shuffle issue

--- src/app/models/deck-model.ts
+++ src/app/models/deck-model.ts
@@ -24,16 +24,20 @@ export class DeckModel {
   }
 
   shuffle(): CardModel[] {
-    let d: CardModel[] = this.initDeck();
-
+    let d = this.deck;
+    console.log('original Deck: ' + JSON.stringify(d));
     const shuffledDeck = [];
     let key;
+    console.log(JSON.stringify(d));
 
     while (shuffledDeck.length < 52) {
-      key = Math.floor((Math.random() * d.length));
+      key = Math.floor((Math.random() * d.length) + 1);
+      key--;
       console.log('key' + key + ': ' + key);
       shuffledDeck.push(d[key]);
-      d.splice(key);
+      d.splice(key, 1);
+      console.log(JSON.stringify(d));
+      console.log('Dlength' + d.length);
     }
 
     console.log('Shuffle: ' + shuffledDeck.length + ' ' + JSON.stringify(shuffledDeck));
